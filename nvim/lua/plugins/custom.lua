fb05f3b5469ca65f7655e897b374d46e3f366795
Ranveer Sequeira <ranveersequeira@gmail.com>
2026-01-07 11:50:23 +0530

    Update custom.lua

--- nvim/lua/plugins/custom.lua
+++ nvim/lua/plugins/custom.lua
@@ -157,6 +157,24 @@ return {
       }
     end,
   },
+  
+  {
+    "christoomey/vim-tmux-navigator",
+    cmd = {
+      "TmuxNavigateLeft",
+      "TmuxNavigateDown",
+      "TmuxNavigateUp",
+      "TmuxNavigateRight",
+      "TmuxNavigatePrevious",
+      "TmuxNavigatorProcessList",
+    },
+    keys = {
+      { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+      { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+      { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+      { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+    },
+  },
 
   -- 5. MASON - Ensure all your tools are installed
 
